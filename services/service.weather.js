a1c962e6cb26fc4c67f6a0cfb68ca9989d49d214
wiliami <wiliaup97@gmail.com>
2024-02-02 17:02:30 -0400

    update code

--- services/service.weather.js
+++ services/service.weather.js
@@ -1,1 +1,28 @@
+import { Router } from 'express';
+import axios from 'axios';
 
+const key_api = "d90ff67156bc289cf5e79d6be28f583f"
+
+const router = Router();
+
+router.get('/', async (req, res, next) => {
+
+    res.header('Access-Control-Allow-Origin', 'http://seufrontend.com');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With , Content-Type, Accept');
+    next();
+    
+    try {
+        const responseApi = await axios.get('https://api.meteorologia.com/dados', {
+            headers: {
+                'Authorization': key_api,
+            },
+        });
+
+        res.json(responseApi.data);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Erro ao obter dados meteorológicos');
+    }
+});
+
+export default router;
--- src/app.js
+++ src/app.js
@@ -1,15 +1,21 @@
 import express from 'express';
-
+import routes from './routes/main.routes.js';
 
 class App {
     constructor() {
         this.app = express();
         this.config();
+        this.routes();
     }
 
     config() {
-        this.app.use(express.urlencoded({extended:false}));
+        this.app.use(express.urlencoded({ extended:false }));
         this.app.use(express.json());
+    }
+
+    routes() {
+        this.app.use('/', routes);
+        this.app.get('*', (req, res) => res.send('Rota 404 - Not found error!:'));
     }
 
     listen(port) {
