fdcb8e7f212e3ebd5872f00687c47cfac9f25041
wiliami <wiliaup97@gmail.com>
2025-01-06 19:09:38 -0400

    feat: the server will read the key private api to auth request
    The server will allows requests only if client be authorized. Use the
    variables enviroment for control the flows and avoid request unwanted

--- services/service.weather.js
+++ services/service.weather.js
@@ -1,24 +1,26 @@
 import { Router } from 'express';
 import axios from 'axios';
+import dotenv from 'dotenv'
+dotenv.config()
 
-const key_api = "d90ff67156bc289cf5e79d6be28f583f"
+const private_key_api = process.env.PRIVATE_KEY
 
 const router = Router();
 
 router.get('/', async (req, res, next) => {
 
-    res.header('Access-Control-Allow-Origin', 'http://seufrontend.com');
+    res.header('Access-Control-Allow-Origin', 'http://localhost:8082');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With , Content-Type, Accept');
     next();
     
     try {
-        const responseApi = await axios.get('https://api.meteorologia.com/dados', {
+        const response = await axios.get('https://api.meteorologia.com/dados', {
             headers: {
-                'Authorization': key_api,
+                'Authorization': private_key_api,
             },
         });
 
-        res.json(responseApi.data);
+        res.json(response.data);
     } catch (error) {
         console.error(error);
         res.status(500).send('Erro ao obter dados meteorológicos');
