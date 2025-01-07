da07ac1b8be82f50bcbc698ac5e599619ff0f3b1
wiliami <wiliaup97@gmail.com>
2025-01-07 08:39:02 -0400

    fix: fixes endpoint for add routes of page server side

--- src/routes/home.js
+++ src/routes/home.js
@@ -3,7 +3,7 @@ import { Router } from 'express';
 const router = Router();
 
 router.get('/', (req, res) => {
-    return res.send('Rota home');
+    return res.render('index');
 });
 
 export default router;
