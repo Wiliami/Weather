61328047eb4377f87fa95d93938de48f354e39a4
wiliami <wiliaup97@gmail.com>
2025-01-17 09:54:52 -0400

    feat: read private key api for auth request client
    read key private API Weather

--- services/service.weather.js
+++ services/service.weather.js
@@ -3,27 +3,26 @@ import axios from 'axios';
 import dotenv from 'dotenv'
 dotenv.config()
 
-const private_key_api = process.env.PRIVATE_KEY
+const private_key = process.env.PRIVATE_KEY
 
 const router = Router();
 
 router.get('/', async (req, res, next) => {
 
-    res.header('Access-Control-Allow-Origin', 'http://localhost:8082');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With , Content-Type, Accept');
-    next();
+    res.header("Access-Control-Allow-Origin", "http://localhost:8082");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With , Content-Type, Accept");
     
     try {
-        const response = await axios.get('https://api.meteorologia.com/dados', {
+        const response = await axios.get("https://api.meteorologia.com/dados", {
             headers: {
-                'Authorization': private_key_api,
-            },
+                "Authorization": private_key,
+            },  
         });
 
         res.json(response.data);
     } catch (error) {
         console.error(error);
-        res.status(500).send('Erro ao obter dados meteorológicos');
+        res.status(500).send("Erro ao obter dados meteorológicos: " + error.message);
     }
 });
 
